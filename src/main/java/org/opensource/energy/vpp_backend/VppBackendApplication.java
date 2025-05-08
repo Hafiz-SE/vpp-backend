96458561516ef4026c892141681acf5952e1d150
M. Hafizur Rahman <h.rahman1995@outlook.com>
2025-05-08 19:34:15 +0600

    init: initialized project

---
+++ src/main/java/org/opensource/energy/vpp_backend/VppBackendApplication.java
@@ -0,0 +1,13 @@
+package org.opensource.energy.vpp_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VppBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VppBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/opensource/energy/vpp_backend/VppBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package org.opensource.energy.vpp_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VppBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
